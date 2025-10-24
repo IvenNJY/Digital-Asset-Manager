c01099bbcc63d9990b8f9d4512c9634ec005855f
INJY <178879836+IvenNJY@users.noreply.github.com>
2025-10-24 13:34:54 +0800

    Allow for Database Names to be in sync with all members

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'testing',
+        'NAME': 'postgress',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': 'localhost',
